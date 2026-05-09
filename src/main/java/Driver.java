9b4bad149b6c85f82ce1673edc9e2983c173bc2f
Tdarrelpa <darrel2208@gmail.com>
2026-05-10 04:54:01 +0700

    First commit to test GitHub CI/CD (Action)
    Update Maven, Settings, Source contents (main and test), and POM files.

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -15,5 +15,37 @@ public class Driver
 		counter.decrement();
 		
 		System.out.println("Count:" + counter.getCount());
+
+		counter.increaseBy(2);
+
+		System.out.println("Count:" + counter.getCount());
+
+		counter.powerBy(2);
+
+		System.out.println("Count:" + counter.getCount());
+
+		counter.triple();
+
+		System.out.println("Count:" + counter.getCount());
+
+		counter.decreaseBy(12);
+
+		System.out.println("Count:" + counter.getCount());
+
+		counter.increment();
+		
+		System.out.println("Count:" + counter.getCount());
+
+		counter.multiplyBy(2);
+
+		System.out.println("Count:" + counter.getCount());
+
+		counter.dividedBy(2);
+
+		System.out.println("Count:" + counter.getCount());
+
+		counter.decrement();
+
+		System.out.println("Count:" + counter.getCount());
 	}
 }
