063321610b07d5fe8671ec7731d0b64fb44b81cc
Tdarrelpa <darrel2208@gmail.com>
2026-05-10 04:54:01 +0700

    First commit to test GitHub CI/CD (Action)
    Update Maven, Settings, source content (main and test), and pom file.

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -11,7 +11,7 @@ public class CounterTest
 	{
 		Counter testCounter = new Counter();
 		
-		for(int i = 0; i <10; i++) {testCounter.increment();}
+		for(int i = 0; i < 10; i++) {testCounter.increment();}
 		
 		testCounter.reset();
 		
@@ -23,7 +23,7 @@ public class CounterTest
 	{
 		Counter testCounter = new Counter();
 		
-		for(int i = 1; i <10; i++) 
+		for(int i = 1; i < 10; i++) 
 		{
 			testCounter.increment();
 			assertEquals(testCounter.getCount(), i);
@@ -35,10 +35,84 @@ public class CounterTest
 	{
 		Counter testCounter = new Counter();
 		
-		for(int i = 1; i <10; i++) 
+		for(int i = 1; i < 10; i++) 
 		{
 			testCounter.decrement();
 			assertEquals(testCounter.getCount(), i * -1);
+		}
+	}
+
+	@Test
+	public void testIncreaseBy()
+	{
+		Counter testCounter = new Counter();
+
+		for (int i = 1; i < 10; i++) 
+		{
+			testCounter.increaseBy(i);
+			assertEquals(45, testCounter.getCount());
+		}
+	}
+
+	@Test
+	public void testDecreaseBy()
+	{
+		Counter testCounter = new Counter();
+
+		for(int i = 1; i < 10; i++) 
+		{
+			testCounter.decreaseBy(i);
+			assertEquals(-43, testCounter.getCount());
+		}
+	}
+
+	@Test
+	public void testMultiplyBy()
+	{
+		Counter testCounter = new Counter();
+
+		for (int i = 1; i < 10; i++) 
+		{
+			testCounter.multiplyBy(i);
+			assertEquals(362880, testCounter.getCount());
+		}
+	}
+
+	@Test
+	public void testDividedBy()
+	{
+		Counter testCounter = new Counter();
+
+		for(int i = 1; i < 10; i++) 
+		{
+			testCounter.dividedBy(i);
+			assertEquals(0.0000027557, testCounter.getCount());
+		}
+	}
+
+	@Test
+	public void testPowerBy()
+	{
+		Counter testCounter = new Counter();
+
+		testCounter.increment();
+		for (int i = 1; i < 10; i++) 
+		{
+			testCounter.powerBy(i);
+			assertEquals(1, testCounter.getCount());
+		}
+	}
+
+	@Test
+	public void testIsCountEven() 
+	{
+		Counter testCounter = new Counter();
+		
+		for(int i = 1; i < 10; i++) 
+		{
+			testCounter.increment();
+			if (i % 2 == 0) {assertTrue(testCounter.isCountEven());} 
+			else {assertFalse(testCounter.isCountEven());}
 		}
 	}
 }
