6a1642a0254ac3f1e47d0f6d4746928646c8fffb
Tdarrelpa <darrel2208@gmail.com>
2026-05-10 04:54:01 +0700

    First commit to test GitHub CI/CD (Action)

---
+++ src/main/java/Counter.java
@@ -0,0 +1,19 @@
+// package src.main.java;
+
+public class Counter 
+{
+	private int count;
+	
+	public Counter() {this.reset();}
+		
+	public void reset() {count = 0;}
+	
+	public void increment() {count++;}
+	
+	public void decrement() {count--;}
+	
+	public int getCount() {return count;}
+
+	@Override
+	protected void finalize() {this.reset();}
+}
